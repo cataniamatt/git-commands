aea5cd64d4e0bdd185ffa8c99d1c385ddcb52fe1
Matthew Catania <32411441+cataniamatt@users.noreply.github.com>
2022-04-27 11:21:19 +0200

    Added comments and invoked the message function

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+# Variable to hold the message to print to the user
 message = "Hello World!"
+
+# Function that prints the message to the user
 def hello():
 	print(message)
+
+# Running the function
+hello()
